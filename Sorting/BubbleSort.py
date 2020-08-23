cc1ec67d90d2d95573dc8ceb11b059301438f825
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-23 12:59:28 -0700

    added BubbleSort.py

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,26 @@
+#sorted from tail
+nums = [6,4,5,3,2]
+#       0 1 2 3 4
+def bubbleSort(arr):
+    for i in range(len(arr)-1):
+        # subtract i bc we know it's sorted from right side each iteration
+        for j in range(len(arr)-1-i):
+
+            # is 6 > 4?, swap
+            # is 6 > 5?, swap
+            # is 6 > 3?, swap
+            # is 6 > 2?, swap arr[3]>arr[4]
+            # [4,5,3,2,6]
+
+            # is 4 > 5?, no
+
+            # is 5 > 3?, swap
+            # is 5 > 2?, swap. don't need to compare to 6 again arr[2]>arr[4]
+            # [4,3,2,5,6]
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+print(bubbleSort(nums))
